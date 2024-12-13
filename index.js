50008f2bf8bb0dedb7f9926fbdcc508ce42c389f
asimov <seyranmustafa4@gmail.com>
2024-12-13 22:19:25 +0300

    p

--- index.js
+++ index.js
@@ -18,6 +18,66 @@ const PORT = process.env?.PORT || 8000;
 require("express-async-errors");
 
 /* ------------------------------------------------------- */
+// //* LOGGER
+// // $ npm i morgan
+// // https://expressjs.com/en/resources/middleware/morgan.html
+
+// const morgan = require('morgan')
+
+// // Morgain is middleware
+// // app.use(morgan('tiny'))
+// // app.use(morgan('short'))
+// // app.use(morgan('dev'))
+// // app.use(morgan('common'))
+// // app.use(morgan('combined'))
+// // Custom Logs:
+// // app.use(morgan('TIME=":date[iso]" - URL=":url" - Method=":method" - IP=":remote-addr" - Ref=":referrer" - Status=":status" - Sign=":user-agent" (:response-time[digits] ms)'))
+
+// // Write to file:
+// // https://nodejs.org/api/fs.html#file-system-flags
+// // const fs = require('node:fs')
+// // app.use(morgan('combined', {
+// //   stream: fs.createWriteStream('./access.log', { flags: 'a+' })
+// // }))
+
+// // Write to file day-by-day:
+// const fs = require('node:fs')
+// const now = new Date()
+// // console.log(now, typeof now)
+// const today = now.toISOString().split('T')[0]
+// // console.log(today)
+// app.use(morgan('combined', {
+//   stream: fs.createWriteStream(`./logs/${today}.log`, { flags: 'a+' })
+// }))
+
+// Moved to file:
+app.use(require('./src/middlewares/logger'))
+
+/* ------------------------------------------------------- */
+//* DOCUMENTATION
+// https://swagger-autogen.github.io/docs/
+// $ npm i swagger-autogen # JSON creator
+// $ npm i swagger-ui-express
+// $ npm i redoc-express
+
+//* JSON
+app.use('/documents/json', (req, res) => {
+  res.sendFile('swagger.json', { root: '.' })
+})
+
+//* SWAGGER
+// https://www.npmjs.com/package/swagger-ui-express
+const swaggerUi = require('swagger-ui-express')
+const swaggerJson = require('./swagger.json')
+app.use('/documents/swagger', swaggerUi.serve, swaggerUi.setup(swaggerJson, { swaggerOptions: { persistAuthorization: true } }))
+
+//* REDOC
+// https://www.npmjs.com/package/redoc-express
+const redoc = require('redoc-express')
+app.use('/documents/redoc', redoc({ specUrl: '/documents/json', title: 'Redoc UI' }))
+
+
+/* ------------------------------------------------------- */
 //db connection
 dbConnection();
 
